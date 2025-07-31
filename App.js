a7d155a85ca4f1dc4477e0cec3ce84dd156dfaf2
SterbeNNn <71097582+SterbeNNn@users.noreply.github.com>
2025-07-31 22:51:37 +0300

    No

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Привет, Kiritoshi!</Text>
+      <Text style={styles.subtitle}>Твой финансовый трекер уже здесь 🔥</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+  },
+});
